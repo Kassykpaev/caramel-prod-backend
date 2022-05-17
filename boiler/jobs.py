b245db58ac5156cc9d703db580d04f988a6f7b41
Mukhammed <kzdoandos@gmail.com>
2022-05-17 21:17:07 +0600

    refactor jobs.py

--- boiler/jobs.py
+++ boiler/jobs.py
@@ -7,85 +7,73 @@ import pytz
 utc = pytz.UTC
 
 
-def update_manual(boiler):
-    if boiler.status == "IN_PROGRESS":
-        timestamp = datetime.datetime.now().replace(tzinfo=utc)
-        if boiler.ending_time_of_iteration.replace(tzinfo=utc) <= timestamp:
-            tmp = boiler.initial_order_volume - boiler.made_volume
-            if tmp > boiler.volume:
-                boiler.made_volume += boiler.volume
-                boiler.ending_time_of_iteration = timestamp + \
-                    datetime.timedelta(milliseconds=MAKING_TIME)
-            else:
-                boiler.made_volume = 0
-                boiler.status = "VACANT"
-                boiler.order.status = Order.READY_FOR_SHIPPING
-                boiler.order.save()
-                boiler.order = None
-        else:
-            if boiler.order.status != Order.IN_PROGRESS:
-                boiler.order.status = Order.IN_PROGRESS
-                boiler.order.save()
+def reset_boiler(boiler):
+    boiler.engine_voltage = 0
+    boiler.coef_i = 0
+    boiler.coef_mv_bar = 0
+    boiler.error_prev = 0
+    boiler.made_volume = 0
+    boiler.status = "VACANT"
+    boiler.order.status = Order.READY_FOR_SHIPPING
+    boiler.order.save()
+    boiler.order = None
 
-        boiler.engine_temperature += DELTA_TEMPERATURE
-    elif boiler.status in ["VACANT", "ALARM", "DONE"]:
-        if boiler.engine_temperature > MIN_TEMPERATURE:
-            boiler.engine_temperature -= DELTA_TEMPERATURE * 10
+
+def check_order_in_progress(boiler):
+    if boiler.order.status != Order.IN_PROGRESS:
+        boiler.order.status = Order.IN_PROGRESS
+        boiler.order.save()
+
+
+def check_for_engine_overheat(boiler):
     if boiler.engine_temperature >= MAX_TEMPERATURE:
         boiler.status = "ALARM"
 
 
-def get_difference(timestamp, ending_time_of_iteration):
-    init = ending_time_of_iteration - \
-        datetime.timedelta(milliseconds=MAKING_TIME)
-    difference = timestamp-init
-    return difference.seconds
+def decrease_temperature(boiler):
+    if boiler.engine_temperature > MIN_TEMPERATURE:
+        boiler.engine_temperature -= DELTA_TEMPERATURE * 10
 
 
-def update_auto(boiler):
-    if boiler.status == "IN_PROGRESS":
+def is_finished(boiler):
+    timestamp = datetime.datetime.now().replace(tzinfo=utc)
+    return boiler.ending_time_of_iteration.replace(tzinfo=utc) <= timestamp
+
+
+def set_next_batch_if_has_more(boiler):
+    tmp = boiler.initial_order_volume - boiler.made_volume
+    if tmp > boiler.volume:
         timestamp = datetime.datetime.now().replace(tzinfo=utc)
-        if boiler.ending_time_of_iteration.replace(tzinfo=utc) <= timestamp:
-            tmp = boiler.initial_order_volume - boiler.made_volume
-            if tmp > boiler.volume:
-                boiler.made_volume += boiler.volume
-                boiler.ending_time_of_iteration = timestamp + \
-                    datetime.timedelta(milliseconds=MAKING_TIME)
-            else:
-                boiler.made_volume = 0
-                boiler.status = "VACANT"
-                boiler.order.status = Order.READY_FOR_SHIPPING
-                boiler.order.save()
-                boiler.order = None
-                boiler.coef_i = 0
-                boiler.coef_mv_bar = 0
-                boiler.error_prev = 0
-        else:
-            if boiler.order.status != Order.IN_PROGRESS:
-                boiler.order.status = Order.IN_PROGRESS
-                boiler.order.save()
+        boiler.made_volume += boiler.volume
+        boiler.ending_time_of_iteration = timestamp + \
+            datetime.timedelta(milliseconds=MAKING_TIME)
+    else:
+        reset_boiler(boiler)
 
-            e = boiler.engine_target_voltage - boiler.engine_voltage
-            p = KP*e
-            i = boiler.coef_i + KI*e*(DELTA_TIME)
-            d = KD*(e-boiler.error_prev)/(DELTA_TIME)
-            boiler.coef_i = i
-            boiler.engine_voltage = boiler.coef_mv_bar+p+i+d
-            boiler.error_prev = e
 
-        boiler.engine_temperature += DELTA_TEMPERATURE
-    elif boiler.status in ["VACANT", "ALARM", "DONE"]:
-        if boiler.engine_temperature > MIN_TEMPERATURE:
-            boiler.engine_temperature -= DELTA_TEMPERATURE * 10
-    if boiler.engine_temperature >= MAX_TEMPERATURE:
-        boiler.status = "ALARM"
+def calculate_pid(boiler):
+    e = boiler.engine_target_voltage - boiler.engine_voltage
+    p = KP*e
+    i = boiler.coef_i + KI*e*(DELTA_TIME)
+    d = KD*(e-boiler.error_prev)/(DELTA_TIME)
+    boiler.coef_i = i
+    boiler.engine_voltage = boiler.coef_mv_bar+p+i+d
+    boiler.error_prev = e
 
 
 def update_models():
     boilers = Boiler.objects.all()
     for boiler in boilers:
-        if boiler.mode == 'MANUAL':
-            update_manual(boiler)
-        else:
-            update_auto(boiler)
+        if boiler.status == "IN_PROGRESS":
+            if is_finished(boiler):
+                set_next_batch_if_has_more(boiler)
+            else:
+                check_order_in_progress(boiler)
+                if boiler.mode == "AUTO":
+                    calculate_pid(boiler)
+            boiler.engine_temperature += DELTA_TEMPERATURE
+        elif boiler.status in ["VACANT", "ALARM", "DONE"]:
+            decrease_temperature(boiler)
+        check_for_engine_overheat(boiler)
+
         boiler.save()
